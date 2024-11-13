fe42f9b6a7f71d0037c51f67dd7b35a0045a5d83
Miroslav Lžičař <miroslav.lzicar@themama.ai>
2024-11-13 13:43:31 +0100

    google tag + deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://mireklzicar.com",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  // Add this if you're not using a custom domain:
+  // base: '/repository-name',
+  integrations: [
+    mdx(), 
+    sitemap(),
+    solidJs(),
+    tailwind({ applyBaseStyles: false }),
+    partytown({config: {forward: ["dataLayer.push"],}})
+  ],
 })
